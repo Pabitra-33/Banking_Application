389a38ff84e15e2f1220181407ff6aaf663f96bd
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-22 20:54:21 +0530

    created a Account mapper class to map the dto data to account and account data to dto

--- src/main/java/com/banking_app/dto/AccountDto.java
+++ src/main/java/com/banking_app/dto/AccountDto.java
@@ -1,5 +1,17 @@
 package com.banking_app.dto;
 
-public class AccountDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AccountDto {
+	
+	private Long id;
+	private String accountHoldername;
+	private double balance;
 }
---
+++ src/main/java/com/banking_app/mapper/AccountMapper.java
@@ -0,0 +1,27 @@
+package com.banking_app.mapper;
+
+import com.banking_app.dto.AccountDto;
+import com.banking_app.entity.Account;
+
+public class AccountMapper {
+
+	public static Account mapToAccount(AccountDto accountDto) {
+		Account account = new Account(
+				accountDto.getId(), 
+				accountDto.getAccountHoldername(), 
+				accountDto.getBalance()
+		);
+
+		return account;
+	}
+
+	public static AccountDto mapToAccountDto(Account account) {
+		AccountDto accountDto = new AccountDto(
+				account.getId(), 
+				account.getAccHolderName(), 
+				account.getBalance()
+		);
+		
+		return accountDto;
+	}
+}
