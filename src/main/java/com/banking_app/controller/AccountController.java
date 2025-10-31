290b514fe7724b7dfd2797698ecbb6529c350a1c
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-31 22:31:50 +0530

    written business logic, and created rest end-points in controller class

--- src/main/java/com/banking_app/controller/AccountController.java
+++ src/main/java/com/banking_app/controller/AccountController.java
@@ -1,5 +1,30 @@
 package com.banking_app.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banking_app.dto.AccountDto;
+import com.banking_app.service.AccountService;
+
+@RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
 
+	private AccountService accountService;
+
+	public AccountController(AccountService accountService) {
+		this.accountService = accountService;
+	}
+	
+	
+	//REST API End-points
+	
+	@PostMapping
+	public ResponseEntity<AccountDto> addAcount(AccountDto accountDto){
+		return new ResponseEntity<>(accountService.createAccount(accountDto),HttpStatus.CREATED);
+	}
+	
 }
--- src/main/java/com/banking_app/service/AccountServiceImpl.java
+++ src/main/java/com/banking_app/service/AccountServiceImpl.java
@@ -4,6 +4,8 @@ package com.banking_app.service;
 import org.springframework.stereotype.Service;
 
 import com.banking_app.dto.AccountDto;
+import com.banking_app.entity.Account;
+import com.banking_app.mapper.AccountMapper;
 import com.banking_app.repository.AccountRepository;
 
 @Service
@@ -17,8 +19,9 @@ public class AccountServiceImpl implements AccountService{
 
 	@Override
 	public AccountDto createAccount(AccountDto accountDto) {
-		
-		return null;
+		Account account = AccountMapper.mapToAccount(accountDto);
+		Account savedAccount = accountRepository.save(account);//saving the account
+		return AccountMapper.mapToAccountDto(savedAccount);
 	}
 
 }
